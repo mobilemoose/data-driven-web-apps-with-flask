93fa744d25cb4b305dce621123af04c572643e41
mobilemoose <mike.duff29@gmail.com>
2025-08-27 06:56:28 -0400

    working through course

---
+++ app/ch06_routing/starter/pypi_org/views/account_views.py
@@ -0,0 +1,37 @@
+import flask
+
+from pypi_org.infrastructure.view_modifiers import response
+from pypi_org.views.home_views import blueprint
+
+
+#################### INDEX ####################
+
+@blueprint.route('/account')
+@response(template_file='account/index.html')
+def index():
+    return {}
+
+#################### REGISTER ####################
+
+@blueprint.route('/account/register', methods=['GET'])
+@response(template_file='account/register.html')
+def register_get():
+    return {}
+
+@blueprint.route('/account/register', methods=['POST'])
+@response(template_file='account/register.html')
+def register_post():
+    return {}
+
+
+#################### LOGIN ####################
+
+@blueprint.route('/account/login', methods=['GET'])
+@response(template_file='account/login.html')
+def login_get():
+    return {}
+
+@blueprint.route('/account/login', methods=['POST'])
+@response(template_file='account/login.html')
+def login_post():
+    return {}
